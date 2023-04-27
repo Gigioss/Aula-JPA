e19eefe88647840f8dc00fcd1a00da897cea34bd
Gigioss <gigiosinicasi@gmail.com>
2023-04-26 22:45:55 -0300

    exemplo
    aula3

--- com.canalgeekdev.cliente/src/main/java/com/canalgeekdev/cliente/Teste.java
+++ com.canalgeekdev.cliente/src/main/java/com/canalgeekdev/cliente/Teste.java
@@ -1,9 +1,9 @@
 package com.canalgeekdev.cliente;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-
-
+import javax.persistence.TypedQuery;
 
 import com.canalgeekdev.cliente.model.Cliente;
 
@@ -15,6 +15,51 @@ public class Teste {
 	private static EntityManager entityManager = entityManagerFactory.createEntityManager();
 	
 	public static void main(String[] args) {
+//		//delete
+//		entityManager.getTransaction().begin();
+//		int idCliente=8;
+//		entityManager.createQuery("delete from Cliente c where id = :idCliente")
+//				.setParameter("idCliente",idCliente)
+//				.executeUpdate();
+//		
+//		entityManager.getTransaction().commit();
+//		entityManager.close();
+
+//		//Update
+//		entityManager.getTransaction().begin();
+//		int idCliente=1;
+//		entityManager.createQuery("update from Cliente c set nome ='asd7' where c.id = :idCliente")
+//				.setParameter("idCliente",idCliente)
+//				.executeUpdate();
+//		
+//		entityManager.getTransaction().commit();
+//		entityManager.close();
+		
+		//select
+//		String jpql="select c from Cliente c";
+//		TypedQuery<Cliente> typedQuery = entityManager.createQuery(jpql,Cliente.class);
+//		List<Cliente> listaCliente= typedQuery.getResultList();
+//		
+//		for(Cliente cliente: listaCliente) {
+//			System.out.println(cliente.getNome());
+//		}
+//		
+//		
+//		String jpql="select c from Cliente c where id= :idCliente";
+//		int idCliente=10;
+//		
+//		TypedQuery<Cliente> typedQuery = entityManager
+//				.createQuery(jpql,Cliente.class)
+//				.setParameter("idCliente",idCliente);
+//		List<Cliente> listaCliente= typedQuery.getResultList();
+//		
+//		for(Cliente cliente: listaCliente) {
+//			System.out.println(cliente.getNome());
+//		}
+//		
+//		
+//		entityManager.close();
+//		entityManagerFactory.close();
 		
 //		//Buscar
 //	    Cliente cliente = entityManager.find(Cliente.class, 1);
@@ -35,15 +80,15 @@ public class Teste {
 //		entityManager.getTransaction().commit();
 	
 		
+//		//modificar
+//		Cliente cliente = new Cliente();
+//		cliente.setId(1);
+//		cliente.setNome("Face modifica");
+//		
+//		entityManager.getTransaction().begin();
+//		entityManager.merge(cliente);
+//		entityManager.getTransaction().commit();
 		
-		Cliente cliente = new Cliente();
-		cliente.setId(1);
-		cliente.setNome("Face modifica");
 		
-		entityManager.getTransaction().begin();
-		entityManager.merge(cliente);
-		entityManager.getTransaction().commit();
-		
-	
 	}
 }
